2bedd92e16c9eedbdf9bddac462ed7a29a4ceb65
ekaterina-kuchinskaya <katerinakuc@mail.ru>
2015-05-14 00:03:36 +0400

    add redirect to created page

--- web/blockers/background.js
+++ web/blockers/background.js
@@ -1,5 +1,3 @@
-//localStorage.bannerMessageImg =
-
 chrome.extension.onRequest.addListener(
     function(request, sender, sendResponse)
     {
@@ -14,3 +12,23 @@ function onBeforeRequestHandler(details) {
     var tabId = details.tabId;
     console.log(tabId);
 }
+
+chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab)
+{
+	console.log(tabId);
+	if (changeInfo.status === "loading")
+	{
+		chrome.tabs.get(tabId, function (tabs)
+		{
+			var urls = URLS_FOR_BLOCKING.URLS.split(",");
+			for (var i = 0; i < urls.length; ++i)
+			{
+				var matchPattern = URLS_FOR_BLOCKING.PROTOCOLS.concat(urls[i]);
+				if (tabs && tabs.url.toString().match(matchPattern))
+				{
+					chrome.tabs.update(tabId, {url:"../htmlPages/redirectPage.html"})
+				}
+			}
+		});
+	}
+});
---
+++ web/elementsForBlocking.js
@@ -0,0 +1,11 @@
+ELEMENTS_FOR_BLOCKING =
+{
+    IFRAME : "iframe",
+    NOINDEX : "noindex"
+};
+
+URLS_FOR_BLOCKING =
+{
+	PROTOCOLS : "http[s]?://",
+	URLS : "\.\*mail.ru\/\.\*"
+};
