4cb36267af1eda137fd65a5e9e532ed63dae78b7
ekaterina-kuchinskaya <katerinakuc@mail.ru>
2015-05-23 21:10:18 +0400

    add decrypting to background script

--- web/blockers/backgroundScript.js
+++ web/blockers/backgroundScript.js
@@ -1,26 +1,25 @@
 chrome.extension.onRequest.addListener(
     function(request, sender, sendResponse)
     {
-        //todo: add action
         sendResponse();
     });
 
 chrome.webRequest.onBeforeRequest.addListener(onBeforeRequestHandler, {urls: ["http://banerator.net/*", "http://*.com/*banerator*", "*://www.adambarth.com/*"]}, ['blocking']);
 
-function onBeforeRequestHandler(details) {
-    console.log("456");
+function onBeforeRequestHandler(details)
+{
     var tabId = details.tabId;
     console.log(tabId);
 }
 
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab)
 {
-	console.log(tabId);
 	if (changeInfo.status === "loading")
 	{
 		chrome.tabs.get(tabId, function (tabs)
 		{
-			var urls = URLS_FOR_BLOCKING.URLS.split(",");
+			var urls = localStorage.getItem("address");
+			urls = CryptoJS.AES.decrypt(urls, "magicKey").toString(CryptoJS.enc.Utf8).split(";");
 			for (var i = 0; i < urls.length; ++i)
 			{
 				var matchPattern = URLS_FOR_BLOCKING.PROTOCOLS.concat(urls[i]);
