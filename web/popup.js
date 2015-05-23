9997bf9a505348d39a034e49a657db10220e4129
ekaterina-kuchinskaya <katerinakuc@mail.ru>
2015-05-23 21:02:24 +0400

    add encrypting to popup

--- web/popup.js
+++ web/popup.js
@@ -15,9 +15,10 @@ function addUrl()
 		url = url.substring(index + 3, url.length);
 	}
 	var urls = localStorage.getItem("address");
+	urls = CryptoJS.AES.decrypt(urls, "magicKey").toString(CryptoJS.enc.Utf8);
 	if(urls.indexOf(url) == -1)
 	{
 		urls = urls.concat(";" + url);
-		localStorage.setItem("address", urls);
+		localStorage.setItem("address", CryptoJS.AES.encrypt(urls, "magicKey"));
 	}
-};
+}
